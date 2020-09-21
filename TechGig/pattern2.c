c58f6e180918a73a3946b6a1dcad725dc6de0abf
Preethi Ann Jacob <preethiannjacob1998@gmail.com>
2020-09-21 17:01:20 +0530

    Create pattern2.c

---
+++ TechGig/pattern2.c
@@ -0,0 +1,52 @@
+/*
+Numerical Rectangle
+Pattern Problem - 2 (100 Marks)
+You need to take an integer input and then draw the pattern according to it. Say for example if you enter 5 then, the pattern should be like this-
+1 1 1 1 1
+2 2 2 2 2
+3 3 3 3 3
+4 4 4 4 4
+5 5 5 5 5
+
+Input Format
+You will take an integer input n from stdin.
+
+Constraints
+1 <= n <= 1000
+
+Output Format
+Your output should be the pattern according to the input which you had entered. 
+
+Sample TestCase 1
+Input
+5
+Output
+1 1 1 1 1
+2 2 2 2 2
+3 3 3 3 3
+4 4 4 4 4
+5 5 5 5 5
+Time Limit(X):
+1.00 sec(s) for each input.
+Memory Limit:
+512 MB
+Source Limit:
+100 KB
+Allowed Languages:
+C, C++, C++11, C++14, C#, Java, Java 8, PHP, PHP 7, Python, Python 3, Perl, Ruby, Node Js, Scala, Clojure, Haskell, Lua, Erlang, Swift, VBnet, Js, Objc, Pascal, Go, F#, D, Groovy, Tcl, Ocaml, Smalltalk, Cobol, Racket, Bash, GNU Octave, Rust, Common LISP, R, Julia, Fortran, Ada, Prolog, Icon, Elixir, CoffeeScript, Brainfuck, Pypy, Lolcode, Nim, Picolisp, Pike
+
+*/
+
+#include<stdio.h>
+int main(int argc, char *a[])
+{	int n;
+	scanf("%d",&n);
+	for(int i=1;i<=n;i++){
+		for(int j=1;j<=n;j++){
+			if(j==n)
+				printf("%d\n",i);
+			else
+				printf("%d ",i);
+		}
+	}
+}
