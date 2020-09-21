266ef6f75a397009c401e8f73bc74acfb0699211
Preethi Ann Jacob <preethiannjacob1998@gmail.com>
2020-09-21 17:33:00 +0530

    Update Asterisk_Rectangle.c

--- TechGig/Asterisk_Rectangle.c
+++ TechGig/Asterisk_Rectangle.c
@@ -43,8 +43,8 @@ int main(int argc, char *a[])
 {	int n;
 	scanf("%d",&n);
 	for(int i=1;i<=n;i++){
-		for(int j=1;j<=n-1;j++){
-			if(j==n-1)
+		for(int j=1;j<=5;j++){
+			if(j==5)
 				printf("*\n");
 			else
 				printf("* ");
