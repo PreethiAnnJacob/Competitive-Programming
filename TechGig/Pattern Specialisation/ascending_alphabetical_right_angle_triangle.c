1942c0c7df3ae72918344a002a1b3d58fb728c3e
Preethi Ann Jacob <preethiannjacob1998@gmail.com>
2020-09-21 18:54:16 +0530

    Create ascending_alphabetical_right_angle_triangle.c

---
+++ TechGig/Pattern Specialisation/ascending_alphabetical_right_angle_triangle.c
@@ -0,0 +1,33 @@
+/*
+Pattern Problem - 14 (100 Marks)
+You need to take an integer input and then draw the pattern according to it. Say for example if you enter 5 then, the pattern should be like this-
+A
+A B
+A B C
+A B C D
+A B C D E 
+
+Input Format
+You will take an integer input n from stdin.
+
+Constraints
+1 <= n <= 26
+
+Output Format
+Your output should be the pattern according to the input which you had entered. 
+*/
+
+/* Read input from STDIN. Print your output to STDOUT*/
+#include<stdio.h>
+int main(int argc, char *a[])
+{	int n;char ch='A';
+	scanf("%d",&n);
+	for(int i=0;i<n;i++){
+		for(int j=0;j<=i;j++){
+			if(j==i)
+				printf("%c\n",(ch+j));
+			else
+				printf("%c ",(ch+j));
+		}
+	}
+}
