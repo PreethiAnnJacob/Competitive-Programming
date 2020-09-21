53d5a89af91fb233b92ce4886640736ebbc2233e
Preethi Ann Jacob <preethiannjacob1998@gmail.com>
2020-09-21 23:43:13 +0530

    Create pattern47.c

---
+++ TechGig/Pattern Specialisation/pattern47.c
@@ -0,0 +1,35 @@
+/*
+Pattern Problem - 47 (100 Marks)
+You need to take an integer input and then draw the pattern according to it. Say for example if you enter 5 then, the pattern should be like this-
+* * * * * * * * *
+   * * * * * * *
+      * * * * *
+         * * *
+            *
+
+Input Format
+You will take an integer input n from stdin.
+
+Constraints
+1 <= n <= 1000
+
+Output Format
+Your output should be the pattern according to the input which you had entered. 
+
+*/
+
+#include<stdio.h>
+#include <stdlib.h>
+int main(int argc, char *a[])
+{	int n;char ch='A';
+	scanf("%d",&n);
+	for(int i=n;i>=1;i--){
+		for(int j=1;j<=n-i;j++)
+			printf("  ");
+		for(int j=1;j<=(2*i-1);j++)
+			if(j==(2*i-1))
+				printf("*\n");
+			else
+				printf("* ");
+	}
+}
