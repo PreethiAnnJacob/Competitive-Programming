a798e5b684253c3371e67c5f370176771ed40deb
Preethi Ann Jacob <preethiannjacob1998@gmail.com>
2020-09-27 14:22:36 +0530

    Create MultiplicationTable.py

---
+++ Misc/MultiplicationTable.py
@@ -0,0 +1,9 @@
+#Python program for MULTIPLICATION TABLE
+num=int(input("Enter any number : "))
+'''for i in range(1,11):
+    print(i,"*",num,'=',(num*i))'''
+i=1
+while(i<11):
+    print(i,"*",num,'=',(num*i))
+    i=i+1
+input("Press Enter to exit")
