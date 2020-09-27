896595d80140114eda56d4547145071d7d4f5487
Preethi Ann Jacob <preethiannjacob1998@gmail.com>
2020-09-27 14:25:38 +0530

    Create name_age.py

---
+++ Misc/name_age.py
@@ -0,0 +1,5 @@
+#Python program to print name and age (user given)
+name=input("Enter the name:")
+age=int(input("Enter the age : "))
+print("The name of the person is",name," and the age is",age)
+input("Press enter to quit")
