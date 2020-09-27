e32e35b249a1a2136240c0a68853d043fb39f374
Preethi Ann Jacob <preethiannjacob1998@gmail.com>
2020-09-27 14:32:48 +0530

    Add files via upload

---
+++ Misc/vowel.py
@@ -0,0 +1,7 @@
+#Python Program to find if alphabet is VOWEL or not
+c=input("Enter any alphabet:")
+if((c=='a') | (c=='e') | (c=='i') | (c=='o') | (c=='u')):
+	print("The alphabet is vowel:",c)
+else:
+	print("The alphabet is consonant:",c)
+input("Press enter to exit")
