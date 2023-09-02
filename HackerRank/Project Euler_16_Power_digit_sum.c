405f2dda1ac642be9d8b28c1b331577b03049e40
Preethi Ann Jacob <preethiannjacob1998@gmail.com>
2023-09-02 10:21:24 +0530

    Update Project Euler_16_Power_digit_sum.c

--- HackerRank/Project Euler_16_Power_digit_sum.c
+++ HackerRank/Project Euler_16_Power_digit_sum.c
@@ -43,5 +43,27 @@ for i in range(int(input())):
     print(sd,end="\n")
 */
 
+//Try 2 in C. 1/10. TC#1-9:Wrong answer
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
+    int t;scanf("%d",&t);
+    for (int a0=0;a0<t;a0++)
+    {   int n; scanf("%d",&n);
+        int num=(int)pow(2,n);
+        int sd=0;
+        while(num>0)
+        {   sd+=num%10;
+            num=num/10;
+        }
+        printf("%d\n",sd);  
+    }
+    return 0;
+}
+
 
 
